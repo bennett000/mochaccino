8d2223fe8c12f41bbb6aff3593e05211d54fa4fa
Pawel Galazka <pawel.galazka1986@gmail.com>
2015-11-02 20:45:02 +0100

    Sinon setup

--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
-import {expect} from 'chai'
+import chai, {expect} from 'chai'
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
 
-describe('something', () => {
-   it('do something', () => {
+describe('moccasin', () => {
+   it('do basic chai assert', () => {
       expect('s').to.be.a('string');
-   })
+   });
+
+   it('do basic sinon assert', () => {
+      let cb = sinon.spy();
+      cb('hello foo');
+      expect(cb).to.have.been.calledWith("hello foo");
+   });
 });
